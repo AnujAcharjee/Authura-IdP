2119722851b8b96f205b52ab05f88ebec1a64367
AnujAcharjee <anujacharjee37@gmail.com>
2026-03-13 11:30:06 +0000

    configured Google indexing and crawler files

--- src/controllers/landing.controller.ts
+++ src/controllers/landing.controller.ts
@@ -6,7 +6,7 @@ import { SERVER_URL } from '../utils/constant.js';
 export class LandingController extends BaseController {
   renderLandingPage = this.handleViewRequest(async (req: Request, res: Response) => {
     res.render('pages/app/landing', {
-      title: 'Landing Page',
+      title: 'Pramaan - Secure Identity Provider',
       serverUrl: SERVER_URL,
       docUrl: ENV.APP_DOC_URL,
     });
